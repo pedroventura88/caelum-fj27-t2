450132a01da490b3441fa51a507ea3eced069e78
Rafael Tavares <rafaeltavares91@gmail.com>
2020-06-13 11:43:15 -0300

    fixing Pageable swagger documentation bug

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.annotation.ApiPageable;
 import br.com.alura.forum.dto.input.TopicSearchDto;
 import br.com.alura.forum.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.topic.domain.Topic;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -24,7 +26,8 @@ public class TopicController {
     }
 
     @GetMapping(value="/api/search")
-    public Page<TopicBriefOutputDto> listarPorStatusCategoria(TopicSearchDto topicSearchDto, Pageable pageable) {
+    @ApiPageable
+    public Page<TopicBriefOutputDto> listarPorStatusCategoria(TopicSearchDto topicSearchDto, @ApiIgnore Pageable pageable) {
         Page<Topic> topicos = topicRepository.findAll(topicSearchDto.toSpecification(), pageable);
         return listFromTopics(topicos);
     }
